1dee2642599d2a9a412d285ded910146f66c8471
MrJohn6774 <45367030+MrJohn6774@users.noreply.github.com>
2023-11-02 16:18:00 +1100

    mpirun for artemis

--- job_parallel.sh
+++ job_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -P COMPENG
-#PBS -l select=1:ncpus=12:mpiprocs=12:mem=2GB
+#PBS -l select=1:ncpus=32:mpiprocs=32:mem=4GB
 #PBS -l walltime=00:05:00
 
 cd "$PBS_O_WORKDIR"
@@ -8,4 +8,4 @@ module load python/3.6.5
 module load gcc
 module load openmpi-gcc
 
-python3 ./mpirun.py
+python3 ./mpirun_artemis.py
